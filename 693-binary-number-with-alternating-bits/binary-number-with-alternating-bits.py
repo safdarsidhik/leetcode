08e852ebeed74929619a6cc2c4d616cf56851546
Safdar Sidhik <safdarsidhik@gmail.com>
2026-02-18 13:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (7.61%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        x=bin(n)[2:]
+        flag = 0
+        for i in range(len(x)-1):
+            if x[i] == x[i+1]:
+                flag = 1
+                break
+        if flag == 0:
+            return True
+        else:
+            return False
+            
+        
+
+        
