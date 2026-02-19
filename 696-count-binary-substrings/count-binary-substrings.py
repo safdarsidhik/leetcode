a80dc89dbe52131269d7563033366cfbaa360473
Safdar Sidhik <safdarsidhik@gmail.com>
2026-02-19 22:23:49 +0530

    Time: 59 ms (57.27%) | Memory: 19.7 MB (50.08%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.py
+++ 696-count-binary-substrings/count-binary-substrings.py
@@ -5,11 +5,13 @@ class Solution:
         strk = 1
 
         for i in range(1, len(s)):
-            if s[i] == s[i - 1]: strk += 1
+            if s[i] == s[i - 1]: 
+                strk += 1
             else:
                 prev = strk
                 strk = 1
 
-            if strk <= prev: res += 1
+            if strk <= prev: 
+                res += 1
 
         return res
